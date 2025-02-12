ae3ffa0fc6c7ead56cbebb1a820c343d659d9152
jasonxue <jason@JasonMac.local>
2025-02-13 00:46:09 +0800

    first

---
+++ environment.zsh
@@ -0,0 +1,1 @@
+export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
---
+++ plugin.zsh
@@ -0,0 +1,2 @@
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma-continuum/fast-syntax-highlighting
